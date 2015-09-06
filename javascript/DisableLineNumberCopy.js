e7390e3e6341a14dcfaeb9ec569351302728c34c
hozhang <hozhang@microstrategy.com>
2015-09-06 13:29:15 +0800

    disable copying line numbers in code pieces

---
+++ javascript/DisableLineNumberCopy.js
@@ -0,0 +1,15 @@
+
+// hide line numbers in code pieces right before we copy the code
+
+$(document).ready(function () {
+	$("code").bind("copy", function () {
+    var innerHtml = this.innerHTML;
+    $(this).find('.lineno').remove();
+    var that = this;
+    setTimeout(function(){
+        $(that).children().remove();
+        that.innerHTML = innerHtml;
+    },0);
+	});
+});
+
